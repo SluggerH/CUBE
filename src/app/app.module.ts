e64f26dac4e3139ba3f32d52f2ba48f103db968c
Maxime B <maxime.baillivet@yahoo.fr>
2022-02-03 14:42:46 +0100

    Insertion menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RessourcesCardComponent } from './ressources-card/ressources-card.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RessourcesCardComponent
+    RessourcesCardComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
